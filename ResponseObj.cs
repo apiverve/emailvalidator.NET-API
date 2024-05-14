3f828a8d3802785d1ff7de453a2d1f3e683d111c
APIVerve Team <hello@apiverve.com>
2024-05-14 15:26:15 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,78 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class smtp
+{
+    [JsonProperty("valid")]
+    public bool valid { get; set; }
+
+    [JsonProperty("reason")]
+    public string reason { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("creationDate")]
+    public object creationDate { get; set; }
+
+    [JsonProperty("domain")]
+    public string domain { get; set; }
+
+    [JsonProperty("email")]
+    public string email { get; set; }
+
+    [JsonProperty("username")]
+    public string username { get; set; }
+
+    [JsonProperty("canConnect")]
+    public bool canConnect { get; set; }
+
+    [JsonProperty("hasTypo")]
+    public bool hasTypo { get; set; }
+
+    [JsonProperty("isValid")]
+    public bool isValid { get; set; }
+
+    [JsonProperty("isMxValid")]
+    public bool isMxValid { get; set; }
+
+    [JsonProperty("isSmtpValid")]
+    public bool isSmtpValid { get; set; }
+
+    [JsonProperty("isRegexValid")]
+    public bool isRegexValid { get; set; }
+
+    [JsonProperty("smtp")]
+    public smtp smtp { get; set; }
+
+    [JsonProperty("isCompanyEmail")]
+    public bool isCompanyEmail { get; set; }
+
+    [JsonProperty("isFreeEmail")]
+    public bool isFreeEmail { get; set; }
+
+    [JsonProperty("isDisposable")]
+    public bool isDisposable { get; set; }
+
+    [JsonProperty("checksum")]
+    public int checksum { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
