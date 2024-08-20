92559f2e00488d302db2de1ed075b8b4901c8223
APIVerve Team <hello@apiverve.com>
2024-08-20 10:51:19 -0500

    Bug Fixes

--- EmailValidatorAPIClient.cs
+++ EmailValidatorAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, emailvalidatorQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, EmailValidatorQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(emailvalidatorQueryOptions options = null)
+        public ResponseObj Execute(EmailValidatorQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(emailvalidatorQueryOptions options)
+        private string constructURL(EmailValidatorQueryOptions options)
         {
             string url = _apiEndpoint;
 
